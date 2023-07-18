89e3512c0159cb6067700e3f597066976c59c3b4
samuelgoddard <hello@samgoddard.co.uk>
2023-07-18 17:14:12 +0100

    more scaffold

--- helpers/seo.config.js
+++ helpers/seo.config.js
@@ -1,12 +1,12 @@
 export default {
-  defaultTitle: 'Boilerplate | Next x Tailwind x Motion',
-  description: 'A simple Next.js starter kit with Tailwind and Framer Motion.',
-  titleTemplate: '%s | Next x Tailwind x Motion',
+  defaultTitle: "Creating A Vibrant City Centre | It's In Nottingham",
+  description: "We are It's in Nottingham. Our passion for Nottingham and commitment to support and promote our members will drive economic growth - creating a vibrant city centre which is a destination of choice for visitors. Through every interaction we will build trust and inspire pride in our city centre, its businesses and its people to create a more attractive, healthier, safer, and greener place to live, work and play.",
+  titleTemplate: "%s | It's In Nottingham",
   openGraph: {
     type: 'website',
     locale: 'en_GB',
-    url: 'https://sg-next-tailwind-motion.vercel.app/',
-    site_name: 'Next x Tailwind x Motion',
+    url: 'https://iin-staging.vercel.app/',
+    site_name: "It's In Nottingham",
   },
   twitter: {
     handle: '@samuelgoddard',
