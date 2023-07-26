a8fe334b425c12aed83904b226ef98edf05978b9
samuelgoddard <hello@samgoddard.co.uk>
2023-07-26 13:26:12 +0100

    basic transitions added

--- components/button.js
+++ components/button.js
@@ -8,7 +8,7 @@ export default function Button({ href, label, bgColor }) {
         <span className="block absolute inset-0 transition-transform ease-out duration-[450ms] translate-y-[105%] group-hover:translate-y-0">{label}</span>
       </div>
 
-      <div className="absolute inset-0 flex items-center justify-center pointer-events-none z-[0] opacity-0 group-hover:opacity-100 group-hover:delay-[0ms] delay-[450ms]">
+      <div className="absolute inset-0 flex items-center justify-center pointer-events-none z-[0] opacity-0 group-hover:opacity-100 group-hover:delay-[0ms] delay-[450ms] transition-opacity ease-out duration-[100ms]">
         <div className={`w-[5px] h-[5px] rounded-full transition-all ease-out duration-[450ms] group-hover:duration-[600ms] group-hover:scale-[75] origin-center ${bgColor ? bgColor : 'bg-[#EBEA33]' }`}></div>
       </div>
     </Link>
