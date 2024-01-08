c4dbf7ac17483f8d0808f12b14171a7872af73dc
samuelgoddard <hello@samgoddard.co.uk>
2024-01-08 13:05:48 +0000

    calendar button

--- pages/about.js
+++ pages/about.js
@@ -218,16 +218,16 @@ export default function AboutUs(initialData) {
 
                     
                     {about.eventCalenderSectionPdfDownload?.asset && (
-                      <Link href="/contact"  className={`a11y-focus rounded-full border border-[#B4C0C6] py-4 lg:py-6 2xl:py-8 px-6 lg:px-8 2xl:px-10 inline-block leading-[1.025] 2xl:text-2xl 2xl:leading-[1.025] mx-auto relative group overflow-hidden text-center w-full lg:w-auto `}>
+                      <a target="_blank" rel="noopener noreferrer" href={about.eventCalenderSectionPdfDownload?.asset.url} className={`a11y-focus rounded-full border border-[#B4C0C6] py-4 lg:py-6 2xl:py-8 px-6 lg:px-8 2xl:px-10 inline-block leading-[1.025] 2xl:text-2xl 2xl:leading-[1.025] mx-auto relative group overflow-hidden text-center w-full lg:w-auto `}>
                         <div className="relative z-[1] overflow-hidden">
-                          <span className="block transition-transform ease-out duration-[450ms] translate-y-none group-hover:translate-y-[-105%]">Get In Touch</span>
-                          <span className="block absolute inset-0 transition-transform ease-out duration-[450ms] translate-y-[105%] group-hover:translate-y-0">Get In Touch</span>
+                          <span className="block transition-transform ease-out duration-[450ms] translate-y-none group-hover:translate-y-[-105%]">View Calendar</span>
+                          <span className="block absolute inset-0 transition-transform ease-out duration-[450ms] translate-y-[105%] group-hover:translate-y-0">View Calendar</span>
                         </div>
 
                         <div className="absolute inset-0 flex items-center justify-center pointer-events-none z-[0] opacity-0 group-hover:opacity-100 group-hover:delay-[0ms] delay-[450ms] transition-opacity ease-out duration-[100ms]">
                           <div className={`w-[5px] h-[5px] rounded-full transition-all ease-out duration-[450ms] group-hover:duration-[1200ms] group-hover:scale-[120] origin-center bg-black/30`}></div>
                         </div>
-                      </Link>
+                      </a>
                     )}
                   </div>
                 </div>
